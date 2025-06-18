3208b8bd9a60245e133c4c67407028d5092ed8c2
prime7 <tarek5701@gmail.com>
2025-06-18 02:01:26 -0600

    added deployment script with github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/finofo/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
